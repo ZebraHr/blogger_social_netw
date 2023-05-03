d75bddaab4a09e28a3eaebac35573bda4e20fc35
Anna Pobedonostseva <zebra9999@rambler.ru>
2023-05-03 18:21:55 +0300

    First check

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -13,7 +13,3 @@ def csrf_failure(request, reason=''):
 
 def server_error(request):
     return render(request, 'core/500.html', status=500)
-
-
-def permission_denied(request, exception):
-    return render(request, 'core/403.html', status=403)
