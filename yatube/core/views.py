8bbad37c887cbaac153c420f96c37c9786fea52b
Anna Pobedonostseva <zebra9999@rambler.ru>
2023-05-04 09:58:51 +0300

    Fixed 403 view name error

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -7,7 +7,7 @@ def page_not_found(request, exception):
     return render(request, 'core/404.html', {'path': request.path}, status=404)
 
 
-def csrf_failure(request, reason=''):
+def permission_denied(request, reason=''):
     return render(request, 'core/403csrf.html')
 
 
